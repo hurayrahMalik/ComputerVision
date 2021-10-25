8af81f588a3acfc56c8efaf0b95d60678db42e76
hurayrah <hurayrah@vt.edu>
2021-10-25 18:17:14 -0400

    connect dots with lines

--- handRecognition.py
+++ handRecognition.py
@@ -7,6 +7,8 @@ cap = cv2.VideoCapture(0) #create video object from cam 0
 mphands = mp.solutions.hands
 hands = mphands.Hands()
 mpDraw = mp.solutions.drawing_utils
+ptime = 0
+ctime = 0
 
 while (True):
     success, img = cap.read() #read frame 
@@ -15,6 +17,12 @@ while (True):
 
     if (results.multi_hand_landmarks):
         for handLms in results.multi_hand_landmarks:
-            mpDraw.draw_landmarks(img, handLms)
+            mpDraw.draw_landmarks(img, handLms, mphands.HAND_CONNECTIONS)
+
+    ctime = time.time()
+    fps = 1/(ctime-ptime)
+    ptime = ctime
+
+    cv2.putText(img, str(int(fps)), (10,70),cv2.FONT_HERSHEY_COMPLEX, 3, (255, 255, 255), 3)
     cv2.imshow("Image", img) 
     cv2.waitKey(1) #wait 1 millisecond 
