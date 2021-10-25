adecf77db2d5029d796fa6c94fe837691635ff92
hurayrah <hurayrah@vt.edu>
2021-10-25 14:02:59 -0400

    draw dots

--- handRecognition.py
+++ handRecognition.py
@@ -1,12 +1,20 @@
-from typing import no_type_check
+
 import cv2
 import mediapipe as mp
 import time 
 
-cap = cv2.VideoCapture(0)
+cap = cv2.VideoCapture(0) #create video object from cam 0
+mphands = mp.solutions.hands
+hands = mphands.Hands()
+mpDraw = mp.solutions.drawing_utils
 
 while (True):
-    success, img = cap.read()
+    success, img = cap.read() #read frame 
+    imgRGB = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+    results = hands.process(imgRGB)
 
-    cv2.imshow("Image", img)
-    cv2.waitKey(1)
+    if (results.multi_hand_landmarks):
+        for handLms in results.multi_hand_landmarks:
+            mpDraw.draw_landmarks(img, handLms)
+    cv2.imshow("Image", img) 
+    cv2.waitKey(1) #wait 1 millisecond 
